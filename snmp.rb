40da47aaf84f792ffd033fbbb6d55ebdaf837830
Aaron Kalin <akalin@martinisoftware.com>
2012-01-06 08:58:48 -0600

    Comment on the nasty code

--- snmp.rb
+++ snmp.rb
@@ -1,9 +1,15 @@
 require 'snmp'
 
-SNMP::Manager.open(host: 'andromeda.local') do |manager|
+ROUTER_NAME='andromeda.local'
+
+# Open SNMP connection
+SNMP::Manager.open(host: ROUTER_NAME) do |manager|
+  # Walk the SNMP Table for Net Addresses, see RFC 1213-MIB
   manager.walk("ipNetToMediaPhysAddress") do |row|
+    # Re-encode the OCTET String that comes back
     addr = row.value.encode.to_s
     mac = []
+    # Loop over each byte to a nicer HEX value
     addr.each_byte do |c|
       if c.to_s(16) == "0"
         mac << "00"
@@ -13,7 +19,9 @@ SNMP::Manager.open(host: 'andromeda.local') do |manager|
         mac << c.to_s(16)
       end
     end
+    # The table returns 2 extra numbers in front, drop 'em
     mac.shift(2)
+    # Squish them together to form a MAC address
     pp mac.join(":")
   end
 end
