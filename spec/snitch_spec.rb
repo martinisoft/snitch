18e1ea025729c2301949741da7abb00597f91422
Aaron Kalin <akalin@martinisoftware.com>
2020-04-01 15:30:09 -0500

    Re-factor test case for multiple MACs
    This proves that we can return a single username when we have multiple
    MAC addresses configured for them and they happen to be on the network
    at the same time.

--- spec/snitch_spec.rb
+++ spec/snitch_spec.rb
@@ -3,32 +3,45 @@ require 'snitch'
 
 describe Snitch do
   describe "#connected_clients" do
-    let(:row) { double 'row', value: double(to_mac: address) }
-    let(:octal) { "\x04\x06p\x81\x05\x06\xB5\xE8" }
-    let(:address) { "70:81:05:06:B5:E8" }
     let(:snitch) { Snitch.new }
+    let(:first_address) { "71:81:05:06:B5:E8" }
+    let(:second_address) { "40:21:00:06:C5:F8" }
+    let(:known_client) { "brian" }
+    let(:first_client) { double 'row', value: double(to_mac: first_address) }
+    let(:second_client) { double 'row', value: double(to_mac: second_address) }
+    let(:known_clients) do
+      {
+        first_address => known_client,
+        second_address => known_client
+      }
+    end
+
+    let(:manager) do
+      double('manager').tap do |manager|
+        allow(manager).to receive(:walk).and_yield(first_client).and_yield(second_client)
+      end
+    end
+
 
     before do
-      allow(YAML).to receive(:load_file).and_return('70:81:05:06:B5:E8' => client)
+      allow(YAML).to receive(:load_file).and_return(known_clients)
     end
 
     subject { snitch.connected_clients }
 
     context "when router is available" do
-      let(:client) { "brian" }
-
-      let(:manager) do
-        double('manager').tap do |manager|
-          allow(manager).to receive(:walk).and_yield(row)
-        end
-      end
-
       before do
         expect(SNMP::Manager).to receive(:open).and_yield(manager)
       end
 
+      context "when there are multiple MACs for a client" do
+        it "returns one client for that MAC" do
+          expect(subject).to eq [known_client]
+        end
+      end
+
       it "returns current client list array" do
-        expect(subject).to eq [client]
+        expect(subject).to eq [known_client]
       end
     end
 
